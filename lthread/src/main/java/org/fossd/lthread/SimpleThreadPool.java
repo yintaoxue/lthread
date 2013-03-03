23c4ccd576bc5a1dc75a3df47fe5d637011985c3
talin <yintaoxue@gmail.com>
2013-03-03 10:31:10 +0800

    init import

---
+++ lthread/src/main/java/org/fossd/lthread/SimpleThreadPool.java
@@ -0,0 +1,13 @@
+package org.fossd.lthread;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleThreadPool 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
